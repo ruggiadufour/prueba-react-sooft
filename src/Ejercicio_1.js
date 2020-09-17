dc111117ce09b57a88c06a59772d5d387fcfc9d8
Ruggia <angelruggia@gmail.com>
2020-09-17 13:25:47 -0300

    algoritmos terminados falta agregar a componentes

--- src/Ejercicio_1.js
+++ src/Ejercicio_1.js
@@ -1,30 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 
 export default function Ejercicio_1() {
     const [myArray, setmyArray] = useState([1,5,2,3,5,2,4,1,2,1]);
-    let histograma = [];
+    const [histograma, sethistograma] = useState([])
 
-    const mostrarHistograma = () =>{
+    useEffect(()=>{
         hacerHistograma();
-        histograma.map((elemento,j)=>{
-            return (<h3 key={j}>{elemento}</h3>)
-        })
-    }
+    },[])
+    
     const hacerHistograma = () =>{
-        for (let i = 0; i < 5; i++) {
+        for (let i = 1; i <= 5; i++) {
             let fila = "";
             for (let j = 0; j < 10; j++) {
                 if(i == myArray[j]){
                     fila = fila + "*";
+                    
                 }
             }
-            histograma.push(fila);   
+        
+            sethistograma((arreglo) => [...arreglo, fila])
+            
         }
     }
     return (
         <div>
             {
-                mostrarHistograma
+                histograma.map((elemento,j)=>(<h3 key={j}>{j+1}:{elemento}</h3>))
             }
         </div>
     )
--- src/Ejercicio_2.js
+++ src/Ejercicio_2.js
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Ejercicio_1() {
     let n = 5;
-    let matriz = [];
+    const [matriz, setmatriz] = useState([])
+    useEffect(()=>{
+        imprimirX();
+    },[])
     const imprimirX = () =>{
         for(let i = 0; i < n; i++) {
             let fila = "";
             let izq = 0;
             let der = n-1;
             for(let j = 0; j < n; j++) {
-                if(j===izq || j===der){
+                if(i===izq || i===der){
                     fila = fila + "X"
                 }else{
                     fila = fila + "_"
                 }
+                izq++;
+                der--;
             }
-            matriz.push(fila);
+            console.log(fila)
+            setmatriz((elemento) => [...elemento, fila]);
         }
     }
 
